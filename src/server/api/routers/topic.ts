394731ebb5f55e204e71c8b59b323e0858650cb8
reuelteodoro <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>
2023-07-19 09:15:53 +1000

    feat(note-taker): main - added topic and note

---
+++ src/server/api/routers/topic.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+
+export const topicRouter = createTRPCRouter({
+  getAll: protectedProcedure.query(({ ctx }) => {
+    return ctx.prisma.topic.findMany({
+      where: {
+        userId: ctx.session.user.id,
+      },
+    });
+  }),
+});
